398eeec914d1cbca61269172743ef4653934b349
Shane Vandegrift <vandesm14@gmail.com>
2023-03-02 16:10:28 -0500

    add a table view

--- src/components/QueryOutput.tsx
+++ src/components/QueryOutput.tsx
@@ -1,7 +1,9 @@
-import { Button } from '@blueprintjs/core';
-import React, { LegacyRef } from 'react';
+import { Button, HTMLTable } from '@blueprintjs/core';
+import React from 'react';
 import ReactJson from 'react-json-view';
 import { flex } from '../compose/styles';
+import { Column, flattenJSON } from '../json';
+import { Tooltip2 } from '@blueprintjs/popover2';
 
 export interface QueryOutputProps {
   result: any;
@@ -10,12 +12,59 @@ export interface QueryOutputProps {
 export function QueryOutput({ result }: QueryOutputProps) {
   const buttonRef = React.useRef<HTMLDivElement>(null);
   const [isTable, setIsTable] = React.useState(false);
+  const [table, setTable] = React.useState<Column[] | null>(null);
 
   const buttonHeight = buttonRef.current?.clientHeight ?? 0;
 
+  React.useEffect(() => {
+    try {
+      if (!Array.isArray(result)) throw new Error('Not an array');
+
+      let newTable = flattenJSON(result);
+      setTable(newTable);
+    } catch {
+      setTable(null);
+    }
+  }, [result]);
+
   return (
     <>
-      {isTable ? null : (
+      {isTable ? (
+        <HTMLTable>
+          <thead
+            style={{
+              position: 'sticky',
+              backgroundColor: '#383E47',
+            }}
+          >
+            <th
+              style={{
+                width: 'min-content',
+              }}
+            ></th>
+            {table?.map((column) => (
+              <th key={column.key}>{column.key}</th>
+            ))}
+          </thead>
+          <tbody>
+            {table?.[0].values.map((_, i) => (
+              <tr>
+                <td
+                  style={{
+                    backgroundColor: '#383E47',
+                  }}
+                  key={0}
+                >
+                  {i + 1}
+                </td>
+                {table?.map((column, i) => (
+                  <td key={i + 1}>{column.values[i]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </HTMLTable>
+      ) : (
         <ReactJson
           src={result}
           theme="google"
@@ -34,16 +83,28 @@ export function QueryOutput({ result }: QueryOutputProps) {
             ...flex.center,
             position: 'absolute',
             width: '100%',
-            top: -buttonHeight - 10,
+            top: isTable ? 0 : -buttonHeight - 10,
           }}
         >
           <div ref={buttonRef}>
             <Button onClick={() => setIsTable(false)} active={isTable}>
               JSON
             </Button>
-            <Button onClick={() => setIsTable(true)} active={!isTable}>
-              Table
-            </Button>
+            <Tooltip2
+              content={
+                table === null ? (
+                  <span>Result is not an array of objects</span>
+                ) : undefined
+              }
+            >
+              <Button
+                onClick={() => setIsTable(true)}
+                active={!isTable}
+                disabled={table === null}
+              >
+                Table
+              </Button>
+            </Tooltip2>
           </div>
         </div>
       </div>
---
+++ src/json.ts
@@ -0,0 +1,31 @@
+export interface Column {
+  key: string;
+  values: (any | undefined)[];
+}
+
+export function flattenJSON(json: Array<Record<string, any>>): Column[] {
+  const columns: { [key: string]: string[] } = {};
+
+  json.forEach((record) => {
+    const { data, ...rest } = record;
+    const flattenedData = Object.keys(data).reduce((acc, key) => {
+      const value = Array.isArray(data[key])
+        ? JSON.stringify(data[key])
+        : data[key].toString();
+      return { ...acc, [`data.${key}`]: value };
+    }, {});
+
+    const allColumns: Record<string, any> = { ...rest, ...flattenedData };
+
+    Object.keys(allColumns).forEach((key) => {
+      if (!columns[key]) {
+        columns[key] = [];
+      }
+      columns[key].push(allColumns[key]);
+    });
+  });
+
+  return Object.keys(columns)
+    .map((key) => ({ key, values: columns[key] }))
+    .sort((a, b) => a.key.localeCompare(b.key));
+}
