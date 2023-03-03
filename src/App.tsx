b45121726c552dd0735119e97837404479c77227
Shane Vandegrift <vandesm14@gmail.com>
2023-03-02 22:41:40 -0500

    use a global navbar instead of modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
-import { JSONModal } from './components/JSONModal';
+import { JSONInput } from './components/JSONInput';
 import { QueryInput } from './components/QueryInput';
 import { R } from './vendor/deno.land/x/ahh@v0.11.0/mod';
 import { flex } from './compose/styles';
 import { QueryOutput } from './components/QueryOutput';
 import Split from 'react-split';
+import { Button, Navbar } from '@blueprintjs/core';
 
 function App() {
   const [json, setJson] = React.useState<object>();
   const [result, setResult] = React.useState({});
+  const [nav, setNav] = React.useState<'input' | 'output'>('input');
 
   const handleJSONChange = (json: string) => {
     const parsed = R.fn(() => JSON.parse(json));
@@ -27,20 +29,49 @@ function App() {
         ...flex.col,
       }}
     >
-      <JSONModal onChange={handleJSONChange} />
-      <Split
-        direction="vertical"
-        cursor="row-resize"
-        style={{
-          ...flex.col,
-          width: '100%',
-          height: '100%',
-          overflow: 'hidden',
-        }}
-      >
-        <QueryInput json={json} onChange={(json) => setResult(json)} />
-        <QueryOutput result={result} json={json} />
-      </Split>
+      <Navbar>
+        <Navbar.Group>
+          <Navbar.Heading>JSON Query</Navbar.Heading>
+          <Navbar.Divider />
+          <Button
+            icon="code"
+            active={nav === 'input'}
+            onClick={() => setNav('input')}
+          >
+            Input
+          </Button>
+          <Button
+            icon="console"
+            active={nav === 'output'}
+            onClick={() => {
+              setNav('output');
+            }}
+          >
+            Output
+          </Button>
+        </Navbar.Group>
+      </Navbar>
+      {nav === 'input' && (
+        <JSONInput
+          onChange={handleJSONChange}
+          onSubmit={() => setNav('output')}
+        />
+      )}
+      {nav === 'output' && (
+        <Split
+          direction="vertical"
+          cursor="row-resize"
+          style={{
+            ...flex.col,
+            width: '100%',
+            height: '100%',
+            overflow: 'hidden',
+          }}
+        >
+          <QueryInput json={json} onChange={(json) => setResult(json)} />
+          <QueryOutput result={result} json={json} />
+        </Split>
+      )}
     </main>
   );
 }
---
+++ src/components/JSONInput.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Button, Text } from '@blueprintjs/core';
+import useLocalStorageState from 'use-local-storage-state';
+import { flex } from '../compose/styles';
+
+interface JSONInputProps {
+  onChange: (json: string) => void;
+  onSubmit: () => void;
+}
+
+export function JSONInput({ onChange, onSubmit }: JSONInputProps) {
+  const [json, setJson] = useLocalStorageState('json', {
+    defaultValue: '',
+  });
+  const [isValid, setIsValid] = React.useState(false);
+
+  const handleJSONChange = (value: string) => {
+    try {
+      JSON.parse(value);
+      setIsValid(true);
+      return true;
+    } catch (e: any) {
+      setIsValid(false);
+      console.error(e);
+      return false;
+    }
+  };
+
+  React.useEffect(() => {
+    // If the value exists in local storage, we don't want to open the modal
+    if (handleJSONChange(json)) {
+      onChange(json);
+    } else {
+      onSubmit();
+    }
+  }, []);
+
+  return (
+    <section>
+      <textarea
+        value={json}
+        onChange={(e) => {
+          setJson(e.target.value);
+          handleJSONChange(e.target.value);
+        }}
+        style={{
+          width: '100%',
+          height: '30rem',
+          resize: 'none',
+        }}
+      />
+      <div
+        style={{
+          ...flex.col,
+          ...flex.center,
+          gap: '1rem',
+          padding: '1rem',
+        }}
+      >
+        <Button
+          intent="primary"
+          text="Let's Go!"
+          disabled={!isValid}
+          onClick={() => {
+            onChange(json);
+            onSubmit();
+          }}
+          large={true}
+        />
+        {isValid ? (
+          <Text style={{ color: '#72CA9B' }}>Valid JSON</Text>
+        ) : json.length > 0 ? (
+          <Text style={{ color: '#FA999C' }}>Invalid JSON</Text>
+        ) : null}
+      </div>
+    </section>
+  );
+}
